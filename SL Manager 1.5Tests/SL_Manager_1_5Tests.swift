fe22f3dc06ff9e8f51b32aaf42026efd18d41bf8
Belotte <Belotte@iBelotte-XV.local>
2015-04-04 15:36:28 +0200

    Initial Commit

---
+++ SL Manager 1.5Tests/SL_Manager_1_5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SL_Manager_1_5Tests.swift
+//  SL Manager 1.5Tests
+//
+//  Created by Belotte on 04/04/2015.
+//  Copyright (c) 2015 Belotte. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SL_Manager_1_5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
